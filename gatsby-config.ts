ea6a2f7fd8bb82c82bad2ab7c47643408d546d7e
Karol Słupiński <karol.slupinski.97@gmail.com>
2025-06-22 23:19:33 +0200

    initizalize app

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,26 +3,33 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Dagna Beauty Salon Site`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "rTZPLXqQYzO_l3SJVST5ArmX5yq_mFD0DU1IwwTnNwI",
-      "spaceId": "knkp3dx4zmp9"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "rTZPLXqQYzO_l3SJVST5ArmX5yq_mFD0DU1IwwTnNwI",
+        spaceId: "knkp3dx4zmp9",
+      },
     },
-    __key: "images"
-  }]
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-postcss",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
 };
 
 export default config;
