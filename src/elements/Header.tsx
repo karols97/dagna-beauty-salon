8b07b480ad3922545b39041bc130341f5b1e57ae
Karol Słupiński <karol.slupinski.97@gmail.com>
2025-07-19 22:34:55 +0200

    [feat]: implement header and top bar (mobile only)

--- src/elements/Header.tsx
+++ src/elements/Header.tsx
@@ -1,5 +1,24 @@
+import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
 
 export const Header = () => {
-  return <div className="p-2 border border-gray-300 m-1 cursor-pointer w-full">Header</div>;
+  return (
+    <div className="flex justify-center items-end bg-gradientImage bg-cover w-full h-[350px] md:h-[417px] xl:h-[784px] font-gloock text-beige">
+      <div className="flex flex-col w-80 md:w-[512px] xl:w-[960px] items-center">
+        <h1 className="text-center text-[36px] h-[86px] xl:text-[96px] xl:h-[230px] tracking-widest">
+          Mobilny salon kosmetyczny
+        </h1>
+        <p className="text-[80px] xl:text-[128px] h-[109px] xl:h-[175px] font-sans text-gray-800">
+          Dagna
+        </p>
+        <div className="w-[311px] md:w-[512px] xl:w-[960px] h-[110px] md:h-[182px] xl:h-[340px]">
+          <StaticImage
+            src="../images/topBarPicture.png"
+            alt="Manicure pictures"
+            placeholder="none"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/elements/TopBar.tsx
+++ src/elements/TopBar.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export const TopBar = () => {
-  return <div className="flex p-2 border border-gray-300 m-1 cursor-pointer">TopBar</div>;
+  return (
+    <div className="grid place-items-center bg-beige cursor-pointer w-full h-[76px]">
+      <h1 className="text-3xl">Dagna</h1>
+      <h2 className="text-[12px] font-light uppercase font-montserrat tracking-[0.5em]">
+        Beauty Salon
+      </h2>
+    </div>
+  );
 };
