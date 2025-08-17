b37f0b028c919f0c4501193bc94211dcef582cc2
Karol Słupiński <karol.slupinski.97@gmail.com>
2025-08-17 20:56:19 +0200

    [fix]: make ElementTitle svg elements responsive

--- src/components/ElementTitle.tsx
+++ src/components/ElementTitle.tsx
@@ -12,8 +12,13 @@ type ElementTitleProps = {
 export const ElementTitle = ({ shadow = false, lining = false, children }: ElementTitleProps) => {
   return (
     <div className="flex content-center relative h-[91px] w-full overflow-visible">
-      {lining && <LiningVector className="absolute left-3" />}
-      {shadow && <LightShadowVector className="absolute bottom-0 left-[52px]" fill="#BFD3EB" />}
+      {lining && <LiningVector className="absolute left-3 md:left-[108px]" />}
+      {shadow && (
+        <LightShadowVector
+          className="absolute bottom-0 left-[52px] md:left-[150px] xl:left-[90px]"
+          fill="#BFD3EB"
+        />
+      )}
       <div className="flex flex-row justify-center items-center w-full">
         <StarVectorIcon className="z-50" />
         <h1 className="text-[36px] xl:text-[40px] font-gloock uppercase tracking-[4px] z-50 text-project-black mx-3">
